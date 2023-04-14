c05639c40cb00970c1a4626eaa8ff7f3183b625b
Pamir Perdeci <pamirperdeci@gmail.com>
2023-04-14 00:53:20 -0400

    handled issues with ages 0 to 9

--- src/App.jsx
+++ src/App.jsx
@@ -14,11 +14,15 @@ function App() {
   const buttons2 = [];
 
   function buttonClickHandler(e) {
-    if(e.target.value < 10) {
-      setRightDigit(e.target.value)
-    } else{
+    if(e.target.name === "0 to 9"){
+
+      setLeftDigit(e.target.value)
+    } else if (e.target.value > 10) {
       setLeftDigit(e.target.value/10)
+    } else if(e.target.value < 10) {
+      setRightDigit(e.target.value)
     }
+
   }
 
   for(let i=0; i<10; i++) {
@@ -26,7 +30,8 @@ function App() {
     buttons1.push({
       name: `${i*10} to ${(i*10)+9}`,
       value: i*10,
-      key: i
+      key: i,
+      digit: "left"
     })
 
     buttons2.push({
@@ -41,24 +46,36 @@ function App() {
   },[leftDigit, rightDigit])
 
   const submitHandler = () => {
+    if(age<10) {
+      setDisplayMessage(`You are ${age*365} days old`)
+
+    }else{
     const dayInAges = age*365;
     setDisplayMessage(`You are ${dayInAges} days old!`)
+    }
+  }
 
+  const restartHandler =() => {
+    setAge(null)
+    setLeftDigit(0)
+    setRightDigit(0)
+    setDisplayMessage("")
   }
   return (
     <div className="App">
-      <h1>{leftDigit}{rightDigit}</h1>
-      <h1>{displayMessage}</h1>
-      <h1>How old are you</h1>
-      {buttons1.map(btn=>{
-        return (<button onClick={buttonClickHandler} key={btn.key} value={btn.value}>{btn.name}</button>)
+      {displayMessage === "" && <h1>{leftDigit===0 ? "_": leftDigit}{ rightDigit===0 ? "_":rightDigit}</h1>}
+      <h1>{displayMessage}</h1> 
+     {(leftDigit === 0 || rightDigit===0) && <h1>How old are you</h1>}
+      {!age && buttons1.map(btn=>{
+        return (<button onClick={buttonClickHandler} name ={ btn.name}key={btn.key} digit={btn.digit} value={btn.value}>{btn.name}</button>)
       })}
       <br></br>
-      {buttons2.map(btn=>{
+      {(leftDigit !==0 || rightDigit !==0) && age<10 && buttons2.map(btn=>{
         return (<button onClick={buttonClickHandler}key={btn.key} value={btn.value}>{btn.name}</button>)
       })}
-
-      <button onClick={submitHandler}>Submit</button>
+ 
+     {displayMessage === "" && <button onClick={submitHandler}>Submit</button> }
+     {displayMessage !=="" && <button onClick={restartHandler}>Restart</button>}
     </div>
   )
 }
